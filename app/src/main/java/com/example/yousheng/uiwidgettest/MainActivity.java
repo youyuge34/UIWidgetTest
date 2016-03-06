f7bcda8a1e8b7557474450f0d8e29a4ce9ee63bc
尤晟 <1197993367@qq.com>
2016-03-06 13:18:28 +0800

    3,6  13:18

--- app/src/main/java/com/example/yousheng/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/example/yousheng/uiwidgettest/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.yousheng.uiwidgettest;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private Button button;
+    private EditText editText;
+    private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        editText = (EditText)findViewById(R.id.editText);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+//                String input= editText.getText().toString();
+//                Toast.makeText(MainActivity.this,input,Toast.LENGTH_SHORT).show();
+//                imageView.setImageResource(R.drawable.saber);
+//                if (progressBar.getVisibility()== View.GONE){
+//                    progressBar.setVisibility(View.VISIBLE);
+//                }else{
+//                    progressBar.setVisibility(View.GONE);
+//                }
+                int progress = progressBar.getProgress();
+                progress = progress+10;
+                progressBar.setProgress(progress);
+                break;
+            default:
+                break;
+        }
     }
 }
